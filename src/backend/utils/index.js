d3ce1c7ceb91450d34a6edf8899f44167f41d6f3
Shreyas Soni <sonishreyas10@gmail.com>
2022-05-28 03:57:13 +0530

    feat: like/unlike feature added

--- src/backend/utils/index.js
+++ src/backend/utils/index.js
@@ -1,26 +1,5 @@
-export {
-	RequireAuth,
-	loginHandler,
-	registerHandler,
-	setValueHandler,
-	setTestHandler,
-	setFocusHandler,
-} from "./authentication";
-export {
-	getAllUsers,
-	getCurrentUser,
-	updateUserData,
-} from "./users-management";
-export {
-	addNewPost,
-	uploadFilesForPost,
-	getFeedPost,
-	getExplorePost,
-	deletePostHandler,
-	editPostHandler,
-} from "./post-management";
-export {
-	getBookmarkDataHandler,
-	addPostToBookmark,
-	removePostFromBookmark,
-} from "./bookmark-management";
+export * from "./authentication";
+export * from "./users-management";
+export * from "./post-management";
+export * from "./bookmark-management";
+export * from "./like-management";
---
+++ src/backend/utils/like-management.js
@@ -0,0 +1,49 @@
+import { db } from "backend/firebase/firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+
+const getLikeDataHandler = async (userId) => {
+	try {
+		const userRef = doc(db, "users", userId);
+		const userSnapshot = await getDoc(userRef);
+		const user = userSnapshot.data();
+		return user?.liked ? user.liked : [];
+	} catch (error) {
+		return error;
+	}
+};
+
+const addPostToLike = async (userId, updatedValue) => {
+	try {
+		const userRef = doc(db, "users", userId);
+		await updateDoc(userRef, updatedValue);
+		return [...updatedValue.liked];
+	} catch (error) {
+		return error;
+	}
+};
+
+const removePostFromLike = async (userId, updatedValue) => {
+	try {
+		const userRef = doc(db, "users", userId);
+		await updateDoc(userRef, updatedValue);
+		return [...updatedValue.liked];
+	} catch (error) {
+		return error;
+	}
+};
+
+const updateLikesData = async (postId, updatedValue) => {
+	try {
+		const postRef = doc(db, "posts", postId);
+		await updateDoc(postRef, updatedValue);
+		return updatedValue;
+	} catch (error) {
+		return error;
+	}
+};
+export {
+	getLikeDataHandler,
+	addPostToLike,
+	removePostFromLike,
+	updateLikesData,
+};
